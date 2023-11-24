7aca4d2adff33e49286a5937f14b3f41292c5727
Brenobn <brenobragabn@gmail.com>
2023-11-24 16:56:08 -0300

    Importing Tag and ButtonText from components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,11 +3,15 @@ import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return(
     <Container>
       <Header />
+
+      <ButtonText title="Excluir nota" />
 
       <Section title="Links úteis" >
         <Links>
@@ -17,7 +21,8 @@ export function Details() {
       </Section>
 
       <Section title="Marcadores">
-
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
       
       <Button title="Voltar"  />
