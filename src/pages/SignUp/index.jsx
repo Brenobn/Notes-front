9775601f1f5bdc7115ed7203bd9871241c726518
Brenobn <brenobragabn@gmail.com>
2023-12-18 21:51:47 -0300

    Importing api and creating new function handleSignUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
+import { api } from "../../services/api";
+
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
@@ -11,6 +13,12 @@ export function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  function handleSignUp() {
+    if(!name || !email || !password) {
+      return alert("preencha todos os campos!");
+    }
+  }
 
   return(
     <Container>
@@ -43,7 +51,7 @@ export function SignUp() {
           onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar"  />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
         <Link to="/" >
           Voltar para o login
