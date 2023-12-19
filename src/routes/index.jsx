f5dfcb6a66a94ea799d947df8505aaa24a28ab3c
Brenobn <brenobragabn@gmail.com>
2023-12-19 17:44:37 -0300

    Accessing user in useAuth and doing a ternary if with routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,9 +5,11 @@ import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const { user } = useAuth(); 
+
   return(
     <BrowserRouter>
-      <AuthRoutes />
+      { user ? <AppRoutes /> : <AuthRoutes /> }
     </BrowserRouter>
   );
 }
