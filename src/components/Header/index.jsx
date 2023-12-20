71933d8c436fda2e49e405696ba38772b94804b2
Brenobn <brenobragabn@gmail.com>
2023-12-20 10:11:34 -0300

    Importing api and accessing the avatar and user in the api

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,20 @@
 import { RiShutDownLine } from 'react-icons/ri';
 import { useAuth } from '../../hooks/auth';
 
+import { api } from '../../services/api';
+
 import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
   return(
     <Container>
       <Profile to="/profile">
         <img 
-          src="https://github.com/Brenobn.png" 
+          src={avatarUrl} 
           alt="Foto de perfil do github" 
         />
 
